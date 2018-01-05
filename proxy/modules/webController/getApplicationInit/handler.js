00cb1f7ec3b825d5876093b5961bd9014b747ba0
CBJ <chenbingjie@propersoft.cn>
2018-01-05 18:18:09 +0800

    [完成][icmp-139][app] 审批页面增加修改功能 (#153)
    *  审批页面增加‘修改’功能（未完）
    
    * 审批页面增加修改功能
    
    * 修改代理数据

--- proxy/modules/webController/getApplicationInit/handler.js
+++ proxy/modules/webController/getApplicationInit/handler.js
@@ -4,5 +4,9 @@ var handler = module.exports = {};
 var common = require('../../common.js');
 
 handler.onPost = function (req, res, data) {
-  common.jsonRes(req, res, '/all');
+  if (data.serviceName != null && data.serviceName != 'undefined') {
+    common.jsonRes(req, res, '/all_2');
+  }else {
+    common.jsonRes(req, res, '/all_1');
+  }
 };
--- proxy/modules/webController/getSubSystemStandardIndex/handler.js
+++ proxy/modules/webController/getSubSystemStandardIndex/handler.js
@@ -5,6 +5,12 @@ var common = require('../../common.js');
 
 handler.onPost = function (req, res, data) {
   if (data.data == null || data.data.webPath == "/demo/leaveApplyForm") {
-    common.jsonRes(req, res, '/all');
+    if (data.processName === 'processName1') {
+      common.jsonRes(req, res, '/all_1');
+    }else if (data.processName === 'processName2') {
+      common.jsonRes(req, res, '/all_2');
+    }else {
+      common.jsonRes(req, res, '/all_3');
+    }  
   }
 };
