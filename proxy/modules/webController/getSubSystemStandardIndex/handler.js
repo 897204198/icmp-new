2fc6692c30557871ab97a215fca7de0326677736
CBJ <chenbingjie@propersoft.cn>
2018-01-15 11:49:22 +0800

    [完美][icmp-149][app] 审批页面 (#162)
    * 任务下达审批
    
    * 修改结构
    
    * 修改
    
    * 工作联系单审批(删除未实现)
    
    * 添加功能，删除功能，提交问题
    
    * 增加标题

--- proxy/modules/webController/getSubSystemStandardIndex/handler.js
+++ proxy/modules/webController/getSubSystemStandardIndex/handler.js
@@ -9,8 +9,20 @@ handler.onPost = function (req, res, data) {
       common.jsonRes(req, res, '/all_1');
     }else if (data.processName === 'processName2') {
       common.jsonRes(req, res, '/all_2');
+    }else if (data.processName === 'processName3') {
+      common.jsonRes(req, res, '/all_3');
+    }else if (data.processName === 'processName4') {
+      common.jsonRes(req, res, '/all_4');
+    }else if (data.processName === 'processName5'){
+      common.jsonRes(req, res, '/all_5');
+    }else if (data.processName === 'processName6') {
+      common.jsonRes(req, res, '/all_6');
+    }else if (data.processName === 'processName7') {
+      common.jsonRes(req, res, '/all_7');
+    }else if (data.processName === 'processName8') {
+      common.jsonRes(req, res, '/all_8');
     }else {
       common.jsonRes(req, res, '/all_3');
-    }  
+    }
   }
 };
