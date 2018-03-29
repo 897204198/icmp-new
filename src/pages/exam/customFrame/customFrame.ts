b19288783e7d5a8075f702ffb915ad57a22d1d3c
CBJ <chenbingjie@propersoft.cn>
2018-03-29 11:50:14 +0800

    传递token (#368)
    [完美][app-im][PEP-394] 添加token

--- src/pages/exam/customFrame/customFrame.ts
+++ src/pages/exam/customFrame/customFrame.ts
@@ -8,9 +8,10 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 export class ExamCustomFramePage {
 
   myURL: SafeUrl = '';
+  token: string = localStorage.getItem('token');
 
   constructor(private sanitizer: DomSanitizer) {
-    let dangerousVideoUrl = 'https://cloud.propersoft.cn/pea/master/#/customFrame/exam';
+    let dangerousVideoUrl = 'https://cloud.propersoft.cn/pea/master/#/customFrame/exam?token=' + this.token;
     this.myURL = this.sanitizer.bypassSecurityTrustResourceUrl(dangerousVideoUrl);
   }
 }
