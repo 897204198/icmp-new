587c1f1f06a1ed0b4abbc22621d823209cb2e721
Buhaiyang <35132645+Buhaiyang@users.noreply.github.com>
2018-05-17 17:41:45 +0800

    srm修改随手拍功能 (#520)

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,8 +82,7 @@ module.exports = function(grunt) {
           {expand: true, cwd: './custom_contents/' + conf.currentProject + '/resources', src: ['**'], dest: './resources/'},
           {expand: true, cwd: './custom_contents/' + conf.currentProject + '/images', src: ['**'], dest: '<%= app.src %>/assets/images/'},
           {expand: true, cwd: './custom_contents/' + conf.currentProject + '/jsons', src: ['**'], dest: '<%= app.src %>/assets/jsons/'},
-          {expand: true, cwd: './custom_contents/' + conf.currentProject + '/pages/login', src: ['**'], dest: '<%= app.src %>/pages/login/'},
-          {expand: true, cwd: './custom_contents/' + conf.currentProject + '/pages/home', src: ['**'], dest: '<%= app.src %>/pages/home/'}
+          {expand: true, cwd: './custom_contents/' + conf.currentProject + '/pages', src: ['**'], dest: '<%= app.src %>/pages/'}
         ]
       }
     },
