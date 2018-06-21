f77796a556037aeef8356dc42038d82cb8017b59
Lym <devilx2015@gmail.com>
2018-06-21 17:39:42 +0800

    考试返回键处理 (#612)

--- src/pages/exam/customFrame/customFrame.ts
+++ src/pages/exam/customFrame/customFrame.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { NavParams, NavController } from 'ionic-angular';
-import { DeviceService, DeviceInfoState } from '../../../app/services/device.service';
+import { NavParams, NavController, Navbar } from 'ionic-angular';
 
 @Component({
   selector: 'page-exam-custom-frame',
@@ -9,26 +8,37 @@ import { DeviceService, DeviceInfoState } from '../../../app/services/device.ser
 })
 export class ExamCustomFramePage {
 
+  @ViewChild(Navbar) navBar: Navbar;
+  // 地址
   myURL: SafeUrl = '';
+  // token
   token: string = localStorage.getItem('token');
-  private isShowHeader: boolean = false;
+  // 标题
+  title: string = '';
+  // 是否是根目录
+  isRoot: boolean = true;
 
-  constructor(
-    private sanitizer: DomSanitizer,
-    public navParams: NavParams,
-    private navCtrl: NavController,
-    private deviceService: DeviceService) {
+  constructor(private sanitizer: DomSanitizer, public navParams: NavParams, private navCtrl: NavController) {
+    this.title = this.navParams.data.name;
     let dangerousVideoUrl = this.navParams.data.data.url + '?token=' + this.token + '&questionnaireNo=' + this.navParams.data.data.questionnaireNo;
     this.myURL = this.sanitizer.bypassSecurityTrustResourceUrl(dangerousVideoUrl);
-    const deviceInfo: DeviceInfoState = this.deviceService.getDeviceInfo();
-    if (deviceInfo.deviceType === 'ios') {
-      this.isShowHeader = true;
-    }
     window.addEventListener('message', event => {
-      if (event.data === 'popPage') {
-        this.navCtrl.pop();
+      if (event.data === 'root') {
+        this.isRoot = true;
+      } else {
+        this.isRoot = false;
       }
     });
+  }
+
+  ionViewDidLoad() {
+    this.navBar.backButtonClick = (e: UIEvent) => {
+      if (this.isRoot) {
+        this.navCtrl.pop();
+      } else {
+        history.back();
+      }
+    };
   }
 
 }
