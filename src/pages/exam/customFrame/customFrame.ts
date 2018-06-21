5995da83f7ae5f036a483d97dfa95460308e4a6e
Lym <devilx2015@gmail.com>
2018-06-21 16:48:53 +0800

    [完成][][app-im] 考试去除顶部导航栏，增加返回方法 (#611)
    * 标题
    
    * 修改考试页面

--- src/pages/exam/customFrame/customFrame.ts
+++ src/pages/exam/customFrame/customFrame.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { NavParams } from 'ionic-angular';
+import { NavParams, NavController } from 'ionic-angular';
+import { DeviceService, DeviceInfoState } from '../../../app/services/device.service';
 
 @Component({
   selector: 'page-exam-custom-frame',
@@ -10,9 +11,24 @@ export class ExamCustomFramePage {
 
   myURL: SafeUrl = '';
   token: string = localStorage.getItem('token');
+  private isShowHeader: boolean = false;
 
-  constructor(private sanitizer: DomSanitizer, public navParams: NavParams) {
+  constructor(
+    private sanitizer: DomSanitizer,
+    public navParams: NavParams,
+    private navCtrl: NavController,
+    private deviceService: DeviceService) {
     let dangerousVideoUrl = this.navParams.data.data.url + '?token=' + this.token + '&questionnaireNo=' + this.navParams.data.data.questionnaireNo;
     this.myURL = this.sanitizer.bypassSecurityTrustResourceUrl(dangerousVideoUrl);
+    const deviceInfo: DeviceInfoState = this.deviceService.getDeviceInfo();
+    if (deviceInfo.deviceType === 'ios') {
+      this.isShowHeader = true;
+    }
+    window.addEventListener('message', event => {
+      if (event.data === 'popPage') {
+        this.navCtrl.pop();
+      }
+    });
   }
+
 }
