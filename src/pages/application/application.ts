f4cdf015cb6982cb211f3fd09726e9fe8bdafae5
Buhaiyang <35132645+Buhaiyang@users.noreply.github.com>
2018-07-16 15:45:57 +0800

    申请页增加searchbox选中带值 (#678)

--- src/pages/application/application.ts
+++ src/pages/application/application.ts
@@ -318,11 +318,11 @@ export class ApplicationPage {
     if (index == null) {
       let multiple: boolean = (item['category'] === 'multi');
       let searchUrl = item['searchUrl'];
-      params = { 'title': item['labelBak'], 'multiple': multiple, 'searchUrl': searchUrl };
+      params = { 'title': item['labelBak'], 'multiple': multiple, 'searchUrl': searchUrl, 'id': this.input[item['model']] };
     } else {
       let multiple: boolean = (itemList['category'] === 'multi');
       let searchUrl = itemList['searchUrl'];
-      params = { 'title': itemList['labelBak'], 'multiple': multiple, 'searchUrl': searchUrl };
+      params = { 'title': itemList['labelBak'], 'multiple': multiple, 'searchUrl': searchUrl, 'id': this.input[item['model']][index][itemList['model']] };
     }
     let modal = this.modalCtrl.create(SearchboxComponent, params);
     modal.onDidDismiss(data => {
