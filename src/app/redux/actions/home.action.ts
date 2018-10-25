6c31cc0b19b19d57ae98bd483aa0c3836662bd4c
denggy <dengguange@gmail.com>
2018-10-25 22:19:22 +0800

    Revert "[完成][app-im]提交代码到pr (#979)" (#981)
    This reverts commit 817ff403fecc22fee5d41848d3df7c5989e6fe5a.

--- src/app/redux/actions/home.action.ts
+++
@@ -1,8 +0,0 @@
-import { Action } from '@ngrx/store';
-export const REPLACE = '[Home] REPLACE';
-// 替换原先的 ImBadage
-export class HomeReplaceBadageAction implements Action {
-  readonly type = REPLACE;
-  constructor(public payload: string) {}
-}
-export type HomeActions = HomeReplaceBadageAction
