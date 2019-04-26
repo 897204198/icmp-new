73fd8cbfe3c1ff6fde6da3f0b577a1752162d309
wukongnalizou <413962833@qq.com>
2019-04-26 10:45:10 +0800

    抓阄更新

--- src/pages/choose/choose.ts
+++ src/pages/choose/choose.ts
@@ -8,35 +8,55 @@ import { WebSocketService } from '../../app/services/webSocket.service';
 })
 export class ChoosePage {
   private condition: boolean = false;
-  private chooseName: string = '准备抽取';
+  private chooseName: string = '正在连接socket';
+  private connect: boolean = false;
+  private slack: boolean = false;
+  private chooseStatus: boolean = false;
+  private loading: boolean = true;
+  private topStatus: boolean = true;
   subObj: any;
   private nameList: object[] = [];
   constructor(private wsService: WebSocketService, private http: Http) {}
   choose() {
+    if (!this.condition) {
+      return false;
+    }
+    this.chooseStatus = true;
     this.wsService.sendMessage('/app/lot', 'lot', () => {
       this.condition = false;
-      this.chooseName = '正在抽取...';
+      this.chooseName = '正在抽签...';
     });
   }
   ionViewDidEnter(): void {
-    if (this.wsService.stompClient) {
+    if (localStorage.getItem('sock') === '1' && this.wsService.stompClient) {
       this.socket();
     } else {
       this.wsService.connection(localStorage.getItem('token'), () => {
-        this.socket();
+        if (localStorage.getItem('sock') === '1') {
+          this.socket();
+        }
+      }, () => {
+        this.chooseName = 'socket连接失败';
+        this.connect = false;
+        this.loading = false;
       });
     }
   }
   socket() {
+    this.connect = true;
+    this.chooseName = '正在连接slack';
     this.wsService.addSubscribe('/topic/lot', (msg) => {
       let value = JSON.parse(msg.body);
       if (value.status === 'PONG') {
         this.condition = true;
+        this.slack = true;
+        this.topStatus = false;
       }
       if (value.sender) {
-        this.chooseName = '抽取完成';
         this.nameList.push(value.sender);
         this.condition = true;
+        this.chooseStatus = false;
+        // this.chooseName = '再次抽签';
       }
     }, (obj) => {
       this.subObj = obj;
